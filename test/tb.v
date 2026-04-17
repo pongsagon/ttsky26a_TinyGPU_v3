75121cde3450a156241b2c647e19fc510bf81d24
Matt Pongsagon <mattdsik@hotmail.com>
2026-04-17 09:27:35 +0700

    Rename module tt_um_example to user_project
    Updated module instantiation to use new naming convention.

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  ttsky26a_TinyGPU_v3 (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
