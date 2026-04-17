8927ec2ff465b6ce57b72d08109d3200880f0a0f
Matt Pongsagon <mattdsik@hotmail.com>
2026-04-17 09:31:58 +0700

    Add files via upload

--- src/project.v
+++ src/project.v
@@ -52,7 +52,7 @@
 
 //`timescale 1ns / 1ps
 
-module ttsky26a_TinyGPU_v3 (
+module tt_um_TinyGPU_v3 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
